689658e92a74478bc90688fce87625a2368b2629
Aarni Koskela <akx@iki.fi>
2021-02-28 00:40:18 +0200

    No more ugly default gradient

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,10 @@ import { useArrayOfObjects } from "./hooks/useArrayOfObjects";
 import ColorStopsAPIContext from "./contexts/ColorStopsAPIContext";
 import { MainAccordion } from "./components/MainAccordion";
 import { MainToolbar } from "./components/MainToolbar";
+import { getRandomGradient } from "./examplePalettes";
 
 function getInitialGradient() {
-  return [
-    { id: "1", position: 0, color: { r: 0, g: 0, b: 0, a: 1 } },
-    { id: "2", position: 0.5, color: { r: 0, g: 1, b: 0, a: 1 } },
-    { id: "3", position: 1, color: { r: 1, g: 1, b: 1, a: 1 } },
-  ];
+  return getRandomGradient(0.1);
 }
 
 function App() {
