564fcd1bdbc6058837ba3bd050708b5761dac8d7
Aarni Koskela <akx@iki.fi>
2021-03-01 13:59:54 +0200

    Clamp Catmull-Rom RGBA

--- src/gradients/splineSampling.ts
+++ src/gradients/splineSampling.ts
@@ -1,5 +1,6 @@
 import { ColorStop } from "../types";
 import { compute5DCatmullRomPoints } from "../math/CatmullRom5";
+import { clamp } from "../utils";
 
 export function catmullRomPoints(
   stops: readonly ColorStop[],
@@ -11,12 +12,12 @@ export function catmullRomPoints(
   );
   return points.map(([position, r, g, b, a], i) => ({
     id: `x${i}`,
-    position,
+    position: clamp(position),
     color: {
-      r,
-      g,
-      b,
-      a,
+      r: clamp(r),
+      g: clamp(g),
+      b: clamp(b),
+      a: clamp(a),
     },
   }));
 }
